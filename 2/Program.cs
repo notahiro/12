5c95a387ea29b3cc9c4f808d061f8984b151ae68
moise <moise@DESKTOP-07HPEI1>
2024-12-09 22:06:21 +0700

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string message = "Everything's gonna be okay";
+        Console.WriteLine(message);
+
+        while (true)
+        {
+            if (Console.KeyAvailable) // Проверяем, была ли нажата клавиша
+            {
+                Console.ReadKey(true); // Читаем нажатую клавишу
+                Console.WriteLine(message); // Выводим сообщение снова
+            }
+        }
+    }
+}
